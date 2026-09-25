265959e699b9309c852d55487b734e81537e2c36
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 11:56:45 +0530

    FirstLastOccurrence

---
+++ src/org/gla/Searching/FirstLastOccurrence.java
@@ -0,0 +1,72 @@
+package org.gla.Searching;
+
+import java.util.Scanner;
+
+public class FirstLastOccurrence {
+    static int firstOccurrence(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int lastOccurrence(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter sorted array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int first = firstOccurrence(arr, target);
+        int last = lastOccurrence(arr, target);
+
+        System.out.println("First occurrence: " + first);
+        System.out.println("Last occurrence: " + last);
+
+        sc.close();
+    }
+}

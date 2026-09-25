17ed04349085189025b2ee800036954741ef9b1b
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 10:29:12 +0530

    FootballTeam

---
+++ src/org/gla/Array/Level1/FootballTeam.java
@@ -0,0 +1,18 @@
+package org.gla.Array.Level1;
+
+import java.util.Scanner;
+public class FootballTeam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0;
+        System.out.println("Enter heights of 11 football players:");
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+        double mean = sum / heights.length;
+        System.out.println("\nMean height of the football team = " + mean);
+        sc.close();
+    }
+}

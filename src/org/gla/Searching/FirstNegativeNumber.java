fd99e6928b3034d371f188f5170ca03b063fc799
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 11:53:34 +0530

    FirstNegativeNumber

---
+++ src/org/gla/Searching/FirstNegativeNumber.java
@@ -0,0 +1,32 @@
+package org.gla.Searching;
+
+import java.util.Scanner;
+
+public class FirstNegativeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("Index of first negative number: " + index);
+
+        sc.close();
+    }
+}

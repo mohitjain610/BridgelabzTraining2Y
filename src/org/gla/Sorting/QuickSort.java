c37d75a4b0b49c102ce7fdb050f9efd5cc70b491
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 11:47:51 +0530

    QuickSort

---
+++ src/org/gla/Sorting/QuickSort.java
@@ -0,0 +1,57 @@
+package org.gla.Sorting;
+import java.util.Scanner;
+
+public class QuickSort {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of products: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter product prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        quickSort(prices, 0, n - 1);
+
+        System.out.println("Sorted Product Prices:");
+        for (int price : prices) {
+            System.out.print(price + " ");
+        }
+
+        sc.close();
+    }
+}

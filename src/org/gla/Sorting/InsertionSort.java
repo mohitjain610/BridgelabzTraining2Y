728436fddf5c08bfc3127f7d1da060e08f4fac51
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 11:46:35 +0530

    InsertionSort

---
+++ src/org/gla/Sorting/InsertionSort.java
@@ -0,0 +1,38 @@
+package org.gla.Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of employees: ");
+        int n = sc.nextInt();
+
+        int[] ids = new int[n];
+
+        System.out.println("Enter employee IDs:");
+        for (int i = 0; i < n; i++) {
+            ids[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            int key = ids[i];
+            int j = i - 1;
+
+            while (j >= 0 && ids[j] > key) {
+                ids[j + 1] = ids[j];
+                j--;
+            }
+
+            ids[j + 1] = key;
+        }
+
+        System.out.println("Sorted Employee IDs:");
+        for (int id : ids) {
+            System.out.print(id + " ");
+        }
+
+        sc.close();
+    }
+}

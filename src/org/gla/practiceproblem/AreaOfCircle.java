8bb5e340b5bbd15af96e60e33e11122ebd148114
Mohit-Jain-27 <mj353534@gmail.com>
2026-09-25 11:14:11 +0530

    AreaOfCircle

---
+++ src/org/gla/practiceproblem/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package org.gla.practiceproblem;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
